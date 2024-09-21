309544e9839aefa2bb31e379170ded83165847ba
UnknownEvan <44023313+ivbachantcev@users.noreply.github.com>
2024-09-21 15:01:19 +0500

    Update Строки.Индексация строк.Практическая.py

--- 01. Строки. Индексация строк/Строки.Индексация строк.Практическая.py
+++ 01. Строки. Индексация строк/Строки.Индексация строк.Практическая.py
@@ -1,7 +1,7 @@
 example = "Топинамбур"
 print(example[0],
       example[-1],
-      example[len(example)// 2:],
+      example[len(example) // 2:],
       example[::-1],
       example[1::2],
       sep='\n'
