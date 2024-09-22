b782e9182b0bf974c81e562ff009425ba5f5bfd0
UnknownEvan <44023313+ivbachantcev@users.noreply.github.com>
2024-09-22 13:31:08 +0500

    Create Переменные.py

---
+++ 02. Переменные/Переменные.py
@@ -0,0 +1,5 @@
+count_tasks = 12
+count_hours = 1.5
+course_name = 'Python'
+count_time_one_task = count_hours / count_tasks 
+print(f'Курс: {course_name}, всего задач: {count_tasks}, затрачено часов: {count_hours}, среднее время выполнения {count_time_one_task} часа.')
