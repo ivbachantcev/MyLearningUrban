d585a92b334b0d787a247e7d82e8c15975a01c7f
UnknownEvan <44023313+ivbachantcev@users.noreply.github.com>
2024-10-08 11:10:41 +0500

    Update Цикл for.py

--- 14.Цикл for/Цикл for.py
+++ 14.Цикл for/Цикл for.py
@@ -2,9 +2,9 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for x in numbers:
+    if x == 1:
+        continue
     for i in range(2, int(x**.5) + 1):
-        if x == 1:
-            continue
         if not x % i:
             not_primes.append(x)
             break
