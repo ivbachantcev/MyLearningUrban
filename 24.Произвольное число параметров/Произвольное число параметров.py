56e5199c3a9332092cd7480e894a55f89ecd3767
UnknownEvan <44023313+ivbachantcev@users.noreply.github.com>
2024-10-15 09:02:48 +0500

    Update Произвольное число параметров.py

--- 24.Произвольное число параметров/Произвольное число параметров.py
+++ 24.Произвольное число параметров/Произвольное число параметров.py
@@ -1,6 +1,6 @@
 def single_root_words(root_word, *other_words):
     # same words как бы сразу возвращается, реши не создавать переменную,
-    # а написатm однострочную функцию, надеюсь так можно
+    # а написать однострочную функцию, надеюсь так можно
     return [word for word in other_words if word.lower() in root_word.lower() or root_word.lower() in word.lower()]
    
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
