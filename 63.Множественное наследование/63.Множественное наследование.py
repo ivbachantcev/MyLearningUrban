76315f81aa5ed9b311e24d2263fd930324e4439d
IvanPC <darbuup55@gmail.com>
2024-11-20 10:28:43 +0500

    йц

--- 63.Множественное наследование/63.Множественное наследование.py
+++ 63.Множественное наследование/63.Множественное наследование.py
@@ -40,7 +40,7 @@ class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        self._cords[2] -= abs(dz) * self.speed / 2
+        self._cords[2] -= abs(dz) * self.speed // 2
 
 
 class PoisonousAnimal(Animal):
