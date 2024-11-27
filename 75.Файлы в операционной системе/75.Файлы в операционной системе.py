283af68329aaba948929a96ddfc20cc3d4994f60
IvanPC <darbuup55@gmail.com>
2024-11-27 11:36:37 +0500

    75.Файлы в операционной системе

---
+++ 75.Файлы в операционной системе/75.Файлы в операционной системе.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+# в директории, где находится скрипт есть ещё папки, можете открыть и посмотреть на них
+directoty = r'.'
+for root, dirs, files in os.walk(directoty):
+    for file in files:
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(filepath)
+        formatted_time = time.strftime('%d.%m.%Y %H:%M', time.localtime(filetime))
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.split(filepath)[0]
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
