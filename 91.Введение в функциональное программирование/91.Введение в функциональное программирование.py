91b8b5f82397adb4f9cee89069b86f1d1f05a8b1
IvanPC <darbuup55@gmail.com>
2024-12-03 11:50:47 +0500

    Списковые, словарные сборки

--- 91.Введение в функциональное программирование/91.Введение в функциональное программирование.py
+++ 91.Введение в функциональное программирование/91.Введение в функциональное программирование.py
@@ -1,5 +1,5 @@
 def apply_all_func(int_list, *functions):
-    return dict([(func.__name__, func(int_list)) for func in functions])
+    return dict((func.__name__, func(int_list)) for func in functions)
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
